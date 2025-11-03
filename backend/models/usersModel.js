af4a6c2586d456576f32d456d8cd17ad8cbfe1f1
aldonajudeson <aldonajudeson@gmail.com>
2025-11-03 11:02:06 +0530

    aldona- backend usersmodel reports updated

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -1,57 +1,59 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const usersSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    role:{
-        type:String,
-        enum:['citizen','official','admin'],
-        default:'citizen',
-    },
-    department:{
-        type:String,
-        default:null,
-    },
-    flagCount:{
-        type:Number,
-        default:0,
-    },
-    status:{
-        type:String,
-        enum:['active','restricted','banned'],
-        default:'active',
-    },
-    bannedReason:{
-        type:String,
-        default:null,
-    },
-    bannedUntil:{
-        type:Date,
-        default:Date.now,
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now,
-    },
-    reportTotal:{
-        type:Number,
-        default:0,
-    },
-    bookMarkedReports:{
+const usersSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["citizen", "official", "admin"],
+    default: "citizen",
+  },
+  department: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'Department',
+    default: null,
+  },
+  flagCount: {
+    type: Number,
+    default: 0,
+  },
+  status: {
+    type: String,
+    enum: ["active", "restricted", "banned"],
+    default: "active",
+  },
+  bannedReason: {
+    type: String,
+    default: null,
+  },
+  bannedUntil: {
+    type: Date,
+    default: Date.now,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  reportTotal: {
+    type: Number,
+    default: 0,
+  },
+  bookMarkedReports: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Reports',
+  },
+});
 
-    },
-})
-
-module.exports=mongoose.model("Users",usersSchema)
+module.exports = mongoose.model("Users", usersSchema);
