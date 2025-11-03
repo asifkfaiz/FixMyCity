fa37716cf6885a7bd2db735fb41b3f2a0162ca27
aldonajudeson <aldonajudeson@gmail.com>
2025-11-03 10:38:06 +0530

    aldona- backend usersmodel created

---
+++ backend/models/usersModel.js
@@ -0,0 +1,57 @@
+const mongoose=require('mongoose')
+
+const usersSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:String,
+        enum:['citizen','official','admin'],
+        default:'citizen',
+    },
+    department:{
+        type:String,
+        default:null,
+    },
+    flagCount:{
+        type:Number,
+        default:0,
+    },
+    status:{
+        type:String,
+        enum:['active','restricted','banned'],
+        default:'active',
+    },
+    bannedReason:{
+        type:String,
+        default:null,
+    },
+    bannedUntil:{
+        type:Date,
+        default:Date.now,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+    reportTotal:{
+        type:Number,
+        default:0,
+    },
+    bookMarkedReports:{
+
+    },
+})
+
+module.exports=mongoose.model("Users",usersSchema)
