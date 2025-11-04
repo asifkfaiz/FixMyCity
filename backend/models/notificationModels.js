4fe9e4066693f3a24cb28fccb573bf8092066aec
asifkfaiz <asifkfaiz@gmail.com>
2025-11-04 10:28:11 +0530

    Asif-backend server noticifationmodel

---
+++ backend/models/notificationModels.js
@@ -0,0 +1,28 @@
+const mongoose =require('mongoose')
+
+const notificationSchema=new mongoose.Schema(
+    {
+        userId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Users',
+        },
+        type:{
+            type:String
+        },
+        reportId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Reports'
+        },message:{
+            type:String
+        },isRead:{
+            type:Boolean
+        },createdAt:{
+            type:Date
+        },link:{
+            type:String
+        }
+
+    }
+)
+
+module.exports=mongoose.model("Notification",notificationSchema)
