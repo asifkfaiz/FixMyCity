e027441a0c2ba67c1c4292bd6e7270cf6eef7be3
asifkfaiz <asifkfaiz@gmail.com>
2025-11-04 17:14:27 +0530

    Asif-backend usermodel changed

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -1,59 +1,64 @@
 const mongoose = require("mongoose");
 
-const usersSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const usersSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["citizen", "official", "admin"],
+      default: "citizen",
+    },
+    department: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Department",
+      default: null,
+    },
+    flagCount: {
+      type: Number,
+      default: 0,
+    },
+    status: {
+      type: String,
+      enum: ["active", "restricted", "banned"],
+      default: "active",
+    },
+    bannedReason: {
+      type: String,
+      default: null,
+    },
+    bannedUntil: {
+      type: Date,
+      default: Date.now,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    reportTotal: {
+      type: Number,
+      default: 0,
+    },
+    bookMarkedReports: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Report",
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["citizen", "official", "admin"],
-    default: "citizen",
-  },
-  department: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref:'Departments',
-    default: null,
-  },
-  flagCount: {
-    type: Number,
-    default: 0,
-  },
-  status: {
-    type: String,
-    enum: ["active", "restricted", "banned"],
-    default: "active",
-  },
-  bannedReason: {
-    type: String,
-    default: null,
-  },
-  bannedUntil: {
-    type: Date,
-    default: Date.now,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  reportTotal: {
-    type: Number,
-    default: 0,
-  },
-  bookMarkedReports: {
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'Reports',
-  },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Users", usersSchema);
+module.exports = mongoose.model("User", usersSchema);
